251323bd73185746b13fe2402d747764e7c0d83f
DeanParrish <parrish.de@gmail.com>
2014-05-01 13:36:27 -0400

    Appling Currency Converstion to ASP Weight

---
+++ Weight/ConvertCurrency.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Template.master" AutoEventWireup="true" CodeFile="ConvertCurrency.aspx.cs" Inherits="CurrencyConversion" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <asp:DropDownList ID="ddlCurrency1" runat="server" DataSourceID="xmlCurrency" >
+        <asp:ListItem></asp:ListItem>
+    </asp:DropDownList>
+    </asp:DropDownList>
+    <asp:XmlDataSource ID="xmlCurrency" runat="server" DataFile="~/App_Data/ExchangeRates.xml" />
+    <asp:TextBox ID="txtAmount" runat="server" />
+    <asp:Button ID="btnSubmit" runat="server" Text="Submit" />
+    <asp:Label ID="lblResult" runat="server" />
+</asp:Content>
+
---
+++ Weight/ConvertCurrency.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ClassLibrary1;
+using System.Xml;
+using System.Xml.Linq;
+
+
+
+
+public partial class CurrencyConversion : System.Web.UI.Page
+{
+
+    ClassLibrary1.CCLibrary xx = new ClassLibrary1.CCLibrary(50, HttpContext.Current.Server.MapPath("~/App_Data/ExchangeRates.xml"));
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        PopulateDDLFromXML(HttpContext.Current.Server.MapPath("~/App_Data/ExchangeRates.xml"), "currency");
+    }
+
+    private void PopulateDDLFromXML(string doc, string attribute)
+    {
+        XDocument xmlDoc = XDocument.Load(doc);
+        ClassLibrary1.XMLMethods xmlMTD = new ClassLibrary1.XMLMethods();
+        xmlDoc = xmlMTD.RemoveAllNamespaces(xmlDoc.ToString());
+        
+        var getAttributes = from att in xmlDoc.Descendants("Cube")
+                            where att.HasAttributes
+                            select att.Attribute(attribute);
+
+        foreach (var item in getAttributes)
+        {
+            ddlCurrency1.Items.Add(item.ToString());   
+        }
+
+
+    }
+}
