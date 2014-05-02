b165b10443c6533aba4c6f24d189e69cf31f4258
DeanParrish <parrish.de@gmail.com>
2014-05-02 12:30:04 -0400

    Progress on Update File and Appending EUR to XML List

--- Weight/ConvertCurrency.aspx.cs
+++ Weight/ConvertCurrency.aspx.cs
@@ -13,21 +13,46 @@ using System.Xml.Linq;
 
 public partial class CurrencyConversion : System.Web.UI.Page
 {
-
+    XDocument xmlDoc = XDocument.Load(HttpContext.Current.Server.MapPath("~/App_Data/ExchangeRates.xml"));
     ClassLibrary1.CCLibrary xx = new ClassLibrary1.CCLibrary(50, HttpContext.Current.Server.MapPath("~/App_Data/ExchangeRates.xml"));
+    string[,] array = new string[,] { { "currency", "EUR" }, {"rate", "1"} };
+
     protected void Page_Load(object sender, EventArgs e)
     {
-        PopulateDDLFromXML(HttpContext.Current.Server.MapPath("~/App_Data/ExchangeRates.xml"), "currency", ddlCurrency1);
-        PopulateDDLFromXML(HttpContext.Current.Server.MapPath("~/App_Data/ExchangeRates.xml"), "currency", ddlCurrency2);
-        xx.UpdateFile();
+        if (!IsPostBack)
+        {
+            PopulateDDLFromXML(HttpContext.Current.Server.MapPath("~/App_Data/ExchangeRates.xml"), "currency", ddlCurrency1);
+            PopulateDDLFromXML(HttpContext.Current.Server.MapPath("~/App_Data/ExchangeRates.xml"), "currency", ddlCurrency2);
+            xx.UpdateFile(HttpContext.Current.Server.MapPath("~/App_Data/ExchangeRates.xml"), "http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
+            AppendElementXML(HttpContext.Current.Server.MapPath("~/App_Data/ExchangeRates.xml"), array);
+        }  
     }
+    private void AppendElementXML(string path, string[,] arr)
+    {
+        XDocument xmlDoc = XDocument.Load(path);
+        var cubeRoot = from root in xmlDoc.Descendants("Cube")
+                       where !root.HasAttributes
+                       select root;
 
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            //XElement parentElemtn = new XElement("Cube");
+            XElement newElement = new XElement("Cube");
+            for (int j = 0; j < arr.GetLength(1); j+=2)
+            {
+                XAttribute att = new XAttribute(arr[i, j].ToString(), arr[i, j + 1]);
+                newElement.Add(att);
+            }
+            xmlDoc.Element("Cube").Add(newElement);
+        }
+        xmlDoc.Save(path);
+    }
     private void PopulateDDLFromXML(string doc, string attribute, DropDownList list)
     {
         XDocument xmlDoc = XDocument.Load(doc);
         ClassLibrary1.XMLMethods xmlMTD = new ClassLibrary1.XMLMethods();
         xmlDoc = xmlMTD.RemoveAllNamespaces(xmlDoc.ToString());
-
+       
         var getAttributes = from att in xmlDoc.Descendants("Cube")
                             where att.HasAttributes
                             orderby att.Attribute(attribute).Value
