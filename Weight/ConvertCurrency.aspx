6661433a5a4cc0681b3f41eb6a03b8037f42363e
DeanParrish <parrish.de@gmail.com>
2014-05-01 22:52:49 -0400

    Implemented Currency to ASP Weight

--- Weight/ConvertCurrency.aspx
+++ Weight/ConvertCurrency.aspx
@@ -3,13 +3,15 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
-    <asp:DropDownList ID="ddlCurrency1" runat="server" DataSourceID="xmlCurrency" >
-        <asp:ListItem></asp:ListItem>
+    <asp:DropDownList ID="ddlCurrency1" runat="server" >
+        <asp:ListItem>Select</asp:ListItem>
     </asp:DropDownList>
+    <asp:DropDownList ID="ddlCurrency2" runat="server">
+        <asp:ListItem>Select</asp:ListItem>
     </asp:DropDownList>
-    <asp:XmlDataSource ID="xmlCurrency" runat="server" DataFile="~/App_Data/ExchangeRates.xml" />
     <asp:TextBox ID="txtAmount" runat="server" />
-    <asp:Button ID="btnSubmit" runat="server" Text="Submit" />
+    <asp:Button ID="btnSubmit" runat="server" Text="Submit" 
+        onclick="btnSubmit_Click" />
     <asp:Label ID="lblResult" runat="server" />
 </asp:Content>
 
--- Weight/ConvertCurrency.aspx.cs
+++ Weight/ConvertCurrency.aspx.cs
@@ -17,24 +17,42 @@ public partial class CurrencyConversion : System.Web.UI.Page
     ClassLibrary1.CCLibrary xx = new ClassLibrary1.CCLibrary(50, HttpContext.Current.Server.MapPath("~/App_Data/ExchangeRates.xml"));
     protected void Page_Load(object sender, EventArgs e)
     {
-        PopulateDDLFromXML(HttpContext.Current.Server.MapPath("~/App_Data/ExchangeRates.xml"), "currency");
+        PopulateDDLFromXML(HttpContext.Current.Server.MapPath("~/App_Data/ExchangeRates.xml"), "currency", ddlCurrency1);
+        PopulateDDLFromXML(HttpContext.Current.Server.MapPath("~/App_Data/ExchangeRates.xml"), "currency", ddlCurrency2);
+        xx.UpdateFile();
     }
 
-    private void PopulateDDLFromXML(string doc, string attribute)
+    private void PopulateDDLFromXML(string doc, string attribute, DropDownList list)
     {
         XDocument xmlDoc = XDocument.Load(doc);
         ClassLibrary1.XMLMethods xmlMTD = new ClassLibrary1.XMLMethods();
         xmlDoc = xmlMTD.RemoveAllNamespaces(xmlDoc.ToString());
-        
+
         var getAttributes = from att in xmlDoc.Descendants("Cube")
                             where att.HasAttributes
-                            select att.Attribute(attribute);
+                            orderby att.Attribute(attribute).Value
+                            select att.Attribute(attribute).Value;
+                            
 
         foreach (var item in getAttributes)
         {
-            ddlCurrency1.Items.Add(item.ToString());   
+            list.Items.Add(item.ToString());
         }
 
 
+    }
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            lblResult.Text = xx.Convert(decimal.Parse(txtAmount.Text), ddlCurrency1.Text, ddlCurrency2.Text).ToString();
+            
+        }
+        catch (Exception ex)
+        {
+            lblResult.Text = "ERROR";
+            
+        }
+        
     }
 }
